fed743c6f589bed63470ec3904486619c4ded00e
Unknown <jtsshieh@outlook.com>
2018-02-24 12:39:29 -0500

    Fixed

--- commands/cureboredom.js
+++ commands/cureboredom.js
@@ -1,28 +1,26 @@
 exports.run = (bot, msg, args) => {
-    if (args[0] === 'list') {
-        msg.channel.send(ideas.join('\n'));
-    };
-    
-    
-    const ideas = [
-        'Pokemon Go to the polls :ballot_box:',
-        'Consuming Maccas :fries:',
-        'Fighting AstralMod :right_facing_fist:',
-        'Joining a team :handshake:',
-        'Have a battle :crossed_swords:',
-      ];
+  const ideas = [
+    'Pokemon Go to the polls :ballot_box:',
+    'Consuming Maccas :fries:',
+    'Fighting AstralMod :right_facing_fist:',
+    'Joining a team :handshake:',
+    'Have a battle :crossed_swords:',
+  ];
 
-      msg.channel.send(ideas[Math.floor(Math.random() * ideas.length)]);
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'cureboredom',
-    description: 'Finds you something to do.',
-    category: 'Fun',
-  };
-  
+  if (args[0] === 'list') {
+    msg.channel.send(ideas.join('\n'));
+  }
+
+  msg.channel.send(ideas[Math.floor(Math.random() * ideas.length)]);
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'cureboredom',
+  description: 'Finds you something to do.',
+  category: 'Fun',
+};
