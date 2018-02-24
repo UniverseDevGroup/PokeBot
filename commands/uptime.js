af21fdf05a5e35951078dff3ce6f5f1435fc93b9
Unknown <jtsshieh@outlook.com>
2018-02-23 21:27:35 -0500

    Fix alee things

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,27 +1,25 @@
-exports.run = (bot, msg, args) => {
+exports.run = (bot, msg) => {
+  let uptime = parseInt(bot.uptime);
+  uptime = Math.floor(uptime / 1000);
+  let uptimeMinutes = Math.floor(uptime / 60);
+  const minutes = uptime % 60;
+  let hours = 0;
+  while (uptimeMinutes >= 60) {
+    hours++;
+    uptimeMinutes = uptimeMinutes - 60;
+  }
+  const uptimeSeconds = minutes % 60;
+  msg.channel.send(':clock3: Pokebot has been up for ' + hours + ' hours, ' + uptimeMinutes + ' minutes, and ' + uptimeSeconds + ' seconds.');
 
-    var uptime = parseInt(client.uptime);
-    uptime = Math.floor(uptime / 1000);
-    var uptimeMinutes = Math.floor(uptime / 60);
-    var minutes = uptime % 60;
-    var hours = 0;
-    while (uptimeMinutes >= 60) {
-        hours++;
-        uptimeMinutes = uptimeMinutes - 60;
-    }
-    var uptimeSeconds = minutes % 60;
-message.channel.send(":clock3: Pokebot has been up for " + hours + " hours, " + uptimeMinutes + " minutes, and " + uptimeSeconds + " seconds.")
+};
 
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'uptime',
-    description: 'uptime.',
-    usage: 'uptime',
-  };
-  
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'uptime',
+  description: 'Get the uptime of the bot.',
+  category: 'Utility',
+};
