d1c03a7fe66984fa2626a26ffe7b876db8ea964b
Unknown <jtsshieh@outlook.com>
2018-02-24 13:41:04 -0500

    ship

--- commands/ship.js
+++ commands/ship.js
@@ -1,15 +1,14 @@
-exports.run = (bot, msg, args) => {
-    msg.channel.send(message.author.username + " x " + message.guild.members.random().displayName + ':cruise_ship:');
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'ship',
-    description: 'Test the luck of your love life! Ships you with another user.',
-    category: 'Fun',
-  };
-  
+exports.run = (bot, msg) => {
+  msg.channel.send(msg.author.username + ' x ' + msg.guild.members.random().displayName + ':cruise_ship:');
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'ship',
+  description: 'Test the luck of your love life! Ships you with another user.',
+  category: 'Fun',
+};
