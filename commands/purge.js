c530a1217172a4a019c4f36d4a81bd924ae158ca
Unknown <jtsshieh@outlook.com>
2018-02-24 10:11:22 -0500

    Purge bugfixes

--- commands/purge.js
+++ commands/purge.js
@@ -1,10 +1,10 @@
 exports.run = async (bot, msg, args) => {
-  msg.delete();
   const user = msg.mentions.users.first();
   const amount = parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1]);
   if (!amount) return msg.reply('How many message shall I delete?');
   if (!amount && !user) return msg.reply('Tell me the user and amount or the just the amount of messages to purge.');
-  if (amount > 98 || amount < 2) return msg.reply('Choose an amount less than 98 and greater than 1');
+  if (amount > 100 || amount < 3) return msg.reply('Choose an amount less than 98 and greater than 1');
+  msg.delete();
   let msgs = await msg.channel.fetchMessages({ limit: amount });
   if (user) {
     const filterBy = user ? user.id : bot.user.id;
