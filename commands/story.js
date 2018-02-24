10d53c4d459a4d253599c45bb95e4b8164690e0c
Unknown <jtsshieh@outlook.com>
2018-02-24 14:25:33 -0500

    prettify story command

--- commands/story.js
+++ commands/story.js
@@ -1,29 +1,28 @@
-exports.run = async (bot, msg, args) => {
-    msg.guild.fetchMembers().then(guild =>
-        {
-membersList = guild.members.array();  
-let selectedUser1 = membersList[Math.floor(Math.random() * membersList.length)].user;
-let selectedUser2 = membersList[Math.floor(Math.random() * membersList.length)].user;
-let selectedUser3 = membersList[Math.floor(Math.random() * membersList.length)].user;
+exports.run = async (bot, msg) => {
+  msg.guild.fetchMembers().then(guild =>
+  {
+    const membersList = guild.members.array();
+    const selectedUser1 = membersList[Math.floor(Math.random() * membersList.length)].user;
+    const selectedUser2 = membersList[Math.floor(Math.random() * membersList.length)].user;
+    const selectedUser3 = membersList[Math.floor(Math.random() * membersList.length)].user;
 
-    let stories = 
+    const stories =
     [
-        `Story 1 Alee says it\'s coming soon!.`,
-        `Story 2 ${selectedUser1.username} jumps into the window then ${selectedUser2.username} saves ${selectedUser1.username} and ${selectedUser3.username} claps.`,
+      'Story 1 Alee says it\'s coming soon!.',
+      `Story 2 ${selectedUser1.username} jumps into the window then ${selectedUser2.username} saves ${selectedUser1.username} and ${selectedUser3.username} claps.`,
     ];
-let storySelected = [Math.floor(Math.random() * stories.length)]; 
-msg.channel.send(stories[storySelected]);
-        });
+    const storySelected = [Math.floor(Math.random() * stories.length)];
+    msg.channel.send(stories[storySelected]);
+  });
 };
 
 exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'story',
-    description: 'Tells you a story.',
-    category: 'Fun',
-  };
-  
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'story',
+  description: 'Tells you a story.',
+  category: 'Fun',
+};
