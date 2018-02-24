99252aef92aeb9157f597dd66bcc368dbe706a7d
Unknown <jtsshieh@outlook.com>
2018-02-24 12:52:55 -0500

    fix join

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,17 @@
 exports.run = async (bot, msg, args) => {
   if (args.length < 1) return msg.reply('Please choose a team to join');
+  if (msg.member.roles.find('name', 'Mystic')) {
+    msg.member.removeRole(msg.guild.roles.find('name', 'Mystic'));
+    msg.reply('You have left team Mystic.');
+  }
+  else if (msg.member.roles.find('name', 'Valor')) {
+    msg.member.removeRole(msg.guild.roles.find('name', 'Valor'));
+    msg.reply('You have left team Valor.');
+  }
+  else if (msg.member.roles.find('name', 'Instinct')) {
+    msg.member.removeRole(msg.guild.roles.find('name', 'Instinct'));
+    msg.reply('You have left team Instinct.');
+  }
   switch(args[0])
   {
   case 'mystic': {
