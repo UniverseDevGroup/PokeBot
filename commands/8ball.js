b3f97d12268bc13c5541f9109f4edd37a02cc1b6
Unknown <jtsshieh@outlook.com>
2018-02-24 13:57:05 -0500

    real 8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,29 +1,40 @@
 exports.run = async (bot, msg, args) => {
-    if (args.length < 1) return msg.reply('You need to ask the 8-ball something for it to respond!');
+  if (args.length < 1) return msg.reply('You need to ask the 8-ball something for it to respond!');
 
-    const responses = [
-        ':8ball: *May the odds ever be in your favor...*',
-        ':8ball: *Definetely not! Did you ever think that this would work?*',
-        ':8ball: *Most definetely*',
-        ':8ball: *Seems probable..*',
-        ':8ball: *Sure, why not?*',
-        ':8ball: *No!*',
-        ':8ball: *Probably.*',
-        ':8ball: *If a sentience can do a backflip, then this can happen!*'
-    ];
+  const responses = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+  ];
 
-    msg.channel.send(responses[Math.floor(Math.random() * responses.length)]);
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: '8ball',
-    description: 'Ask the magic 8-ball something. It will answer back, and be as much of a smart-alac as it wants to.',
-    usage: '<...question>',
-    category: 'Fun',
-  };
-  
+  msg.channel.send(':8ball ' + (responses[Math.floor(Math.random() * responses.length)]));
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: '8ball',
+  description: 'Ask the magic 8-ball something. It will answer back, and be as much of a smart-alac as it wants to.',
+  usage: '<...question>',
+  category: 'Fun',
+};
