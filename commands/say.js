87c746aae5e1d825117d584d3d2d260778623707
Unknown <jtsshieh@outlook.com>
2018-02-24 10:32:46 -0500

    Say

--- commands/say.js
+++ commands/say.js
@@ -1,21 +1,20 @@
 exports.run = async (bot, msg, args) => {
-    if (!['242775871059001344', '247221105515823104', '236279900728721409'].includes(msg.author.id)) {
-        msg.reply('Nope! You need the person who created this bot to use this command.');
-      }
-    else {
-        message.channel.sendMessage(args.join(" "));
-        message.delete();
-    }
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'say',
-    description: 'Control on what the bot says.',
-    category: 'Owners Only',
-  };
-  
+  if (!['242775871059001344', '247221105515823104', '236279900728721409'].includes(msg.author.id)) {
+    msg.reply('Nope! You need the person who created this bot to use this command.');
+  }
+  else {
+    msg.channel.sendMessage(args.join(' '));
+    msg.delete();
+  }
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'say',
+  description: 'Control on what the bot says.',
+  category: 'Owners Only',
+};
