3d36bcf0591f2694c3a2472a157ecca0de8d35fc
Unknown <jtsshieh@outlook.com>
2018-02-24 14:28:47 -0500

    Hopefully it works

--- events/ready.js
+++ events/ready.js
@@ -1,23 +1,21 @@
-function setGame(bot) {
-  const games = [
-    'Pokemon',
-    'Catching things',
-    'Finding pokemon',
-    'Type p:help for help',
-    'Fighting AstralMod',
-  ];
-
-  bot.user.setPresence({
-    status: 'online',
-    afk: false,
-    game: {
-      type: 0,
-      name: games[Math.floor(Math.random() * games.length)],
-    },
-  });
-}
 module.exports = (bot) => {
   console.log('PokeBot has finished loading.');
-  setGame(bot);
-  bot.setInterval(setGame(bot), 200000);
+  bot.setInterval(function() {
+    const games = [
+      'Pokemon',
+      'Catching things',
+      'Finding pokemon',
+      'Type p:help for help',
+      'Fighting AstralMod',
+    ];
+
+    bot.user.setPresence({
+      status: 'online',
+      afk: false,
+      game: {
+        type: 0,
+        name: games[Math.floor(Math.random() * games.length)],
+      },
+    });
+  }, 200000);
 };
