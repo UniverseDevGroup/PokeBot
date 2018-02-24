67b55e709288a2cd4f9735cb942abfde9243c211
Alee <alee14498@gmail.com>
2018-02-23 21:40:08 -0500

    Added an option to contribute

---
+++ commands/contribute.js
@@ -0,0 +1,16 @@
+exports.run = (bot, msg) => {
+    msg.channel.send('Want to help the bot? Here you go: https://github.com/PokeWorld/PokeBot.');
+  
+  };
+  
+  exports.conf = {
+    aliases: [],
+    guildOnly: true,
+  };
+  
+  exports.help = {
+    name: 'contribute',
+    description: 'Contributing to the bot.',
+    category: 'Utility',
+  };
+  
