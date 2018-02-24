4b7d6d983362048e2f3f12c451531c67cfe87be8
Unknown <jtsshieh@outlook.com>
2018-02-23 21:59:46 -0500

    Tiny little thing

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ exports.run = (bot, msg, args) => {
   if (!msg.member.hasPermission('KICK_MEMBERS')) return msg.reply('You don\'t have permssion to kick members');
   if (!msg.guild.member(bot.user).hasPermission('KICK_MEMBERS')) return msg.reply('I don\'t have permssion to kick members');
   const member = msg.mentions.members.first();
-  member.kick(args.join(' ').slice(3 + member.user.id.length)).then(() => {
+  member.kick(msg.author.tag + ': ' + args.join(' ').slice(3 + member.user.id.length)).then(() => {
     msg.channel.send(`Alright, I kicked **${member.user.tag}** for the reason **${args.join(' ').slice(3 + member.user.id.length)}**`);
   });
 };
