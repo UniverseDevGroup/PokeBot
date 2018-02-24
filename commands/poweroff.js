1caa99de4aaa890b64f90399387adf365b54c80a
Unknown <jtsshieh@outlook.com>
2018-02-23 21:58:27 -0500

    FInished poweroff

--- commands/poweroff.js
+++ commands/poweroff.js
@@ -1,23 +1,21 @@
 exports.run = (bot, msg) => {
-    if (msg.author.id !== (
-        "242775871059001344", "247221105515823104"
-    )); 
-    msg.reply('Nope! You need the person who created this bot to use this command.')
-    else {
-    msg.reply(":warning: Pokebot is now powering off!")
-    console.log('Pokebot is now powering off...')
-    process.exit(0)
-    }
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'poweroff',
-    description: 'Powers off the bot.',
-    category: 'Owners Only',
-  };
-  
+  if (!['242775871059001344', '247221105515823104', '236279900728721409'].includes(msg.author.id)) {
+    msg.reply('Nope! You need the person who created this bot to use this command.');
+  }
+  else {
+    msg.reply(':warning: Pokebot is now powering off!');
+    console.log('Pokebot is now powering off...');
+    process.exit(0);
+  }
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'poweroff',
+  description: 'Powers off the bot.',
+  category: 'Owners Only',
+};
