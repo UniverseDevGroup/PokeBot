1d80e28aac0da59efc73fc29d3fb3925f893c29d
Unknown <jtsshieh@outlook.com>
2018-02-24 11:56:04 -0500

    Warn

--- commands/purge.js
+++ commands/purge.js
@@ -1,12 +1,15 @@
 exports.run = async (bot, msg, args) => {
-  if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You don\'t have permssion to manage messages');
-  if (!msg.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) return msg.reply('I don\'t have permssion to manage messages');
+  if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You don\'t have permssion to manage messages.');
+  if (!msg.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) return msg.reply('I don\'t have permssion to manage messages.');
+
   const user = msg.mentions.users.first();
   const amount = parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1]);
+
   if (!amount) return msg.reply('How many message shall I delete?');
   if (!amount && !user) return msg.reply('Tell me the user and amount or the just the amount of messages to purge.');
   if (amount > 100 || amount < 3) return msg.reply('Choose an amount less than 98 and greater than 1');
   msg.delete();
+
   let msgs = await msg.channel.fetchMessages({ limit: amount });
   if (user) {
     const filterBy = user ? user.id : bot.user.id;
--- commands/warn.js
+++ commands/warn.js
@@ -1,30 +1,31 @@
 exports.run = (bot, msg, args) => {
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(':warning: You cannot warn members due to your permission level.');
-    let warnReason = args.slice(1);
-    var victim = message.mentions.users.first();
+  if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You don\'t have permssion to warn.');
+  const warnReason = args.slice(1);
+  const victim = msg.mentions.users.first();
 
-    message.channel.send(`Successfully logged ${victim.user.tag}'s warning.`)
+  msg.channel.send(`Successfully logged ${victim.user.tag}'s warning.`);
 
-    bot.channels.find('name', 'logs').send(
-        new Discord.RichEmbed()
-          .setColor(0x00ae86)
-          .setTitle('Warning')
-          .addField('ID', victim.id, true)
-          .addField('Created Account', victim.user.createdAt, true)
-          .addField('Warning Reason', warnReason)
-          .setTimestamp()
-          .setFooter('Warned by: ' + message.author.user.tag, message.author.user.avatarURL)
-      );
-  };
-  
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'warn',
-    description: 'Logs a warning to the user.',
-    category: 'Moderation',
-  };
-  
+  const { RichEmbed } = require('discord.js');
+  bot.channels.find('name', 'logs').send(
+    new RichEmbed()
+      .setColor(0x00ae86)
+      .setTitle('Warning')
+      .addField('ID', victim.id, true)
+      .addField('Created Account', victim.user.createdAt, true)
+      .addField('Warning Reason', warnReason)
+      .setTimestamp()
+      .setFooter('Warned by: ' + msg.author.user.tag, msg.author.user.avatarURL)
+  );
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'warn',
+  description: 'Logs a warning to the user.',
+  usage : '@<user> <reason>',
+  category: 'Moderation',
+};
