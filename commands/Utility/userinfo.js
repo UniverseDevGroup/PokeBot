3d56a3964fdd7ce3258475f04693a6ca92a41b34
Unknown <jtsshieh@outlook.com>
2018-02-24 19:54:10 -0500

    fix the trey error

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -1,24 +1,22 @@
-exports.run = async (bot, msg, args) => {
-    const user = msg.mentions.members.first();
+exports.run = async (bot, msg) => {
+  const user = msg.mentions.members.first();
+  const { RichEmbed } = require('discord.js');
+  msg.channel.send(
+    new RichEmbed()
+      .setTitle(`Information for ${user.user.tag}`)
+      .addField('User ID', user.id)
+      .addField('Account Creation Date', user.user.createdAt)
+      .addField('Join Guild Date', user.joinedAt)
+  );
+};
 
-    msg.channel.send(
-        new RichEmbed()
-            .setTitle(`Information for ${user.user.tag}`)
-            .addField('User ID', user.id)
-            .addField('Account Creation Date', member.user.createdAt)
-            .addField('Join Guild Date', user.joinedAt)
-            .addField('Ban Count', 'This user has been banned from ' + parseInt(banCounts[user.id]) + ' guilds.')
-    );
-  };
-  
-  exports.conf = {
-    aliases: ['uinfo', 'userinformation'],
-    guildOnly: true,
-  };
-  
-  exports.help = {
-    name: 'userinfo',
-    description: 'Shows information about the mentioned user',
-    usage: '@user',
-  };
-  
+exports.conf = {
+  aliases: ['uinfo', 'userinformation'],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'userinfo',
+  description: 'Shows information about the mentioned user',
+  usage: '@user',
+};
