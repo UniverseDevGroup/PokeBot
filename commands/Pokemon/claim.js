e12f2cbace0c67631afab45a0d34360eb6e73fe7
Unknown <jtsshieh@outlook.com>
2018-02-24 21:25:23 -0500

    try this

--- commands/Pokemon/claim.js
+++ commands/Pokemon/claim.js
@@ -2,11 +2,11 @@ exports.run = async (bot, msg) => {
   if (!msg.channel.topic.startsWith('Current Owner:')) return msg.reply('Go into one of the gym channels and try again.');
   if (msg.channel.topic == 'Current Owner: *none*') {
     msg.reply('Alright, you have claimed this gym as yours! Be ready to battle anyone who approaches you');
-    msg.channel.setTopic('Current Owner: ' + msg.author.id + '/' + msg.author.tag);
+    msg.channel.setTopic('Current Owner: ' + msg.author.tag + '/' + msg.author.id);
   }
   else {
-    const owner = msg.channel.topic.slice(15).substring(0, msg.author.id.length);
-    msg.channel.send('<@' + owner + '>, come here as ' + msg.author + ' wants to battle you.');
+    const owner = msg.channel.topic.slice(15).substring(msg.author.tag.length);
+    msg.channel.send('<@' + owner + '>, come here as ' + msg.author.displayName + ' wants to battle you.');
   }
 };
 
