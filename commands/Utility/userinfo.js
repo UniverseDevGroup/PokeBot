f3fbfe2eb4ac59a9966659d0257f43be55e648f5
pokesudenk <thisis@notarealema.il>
2018-02-24 19:52:20 -0500

    userinfo comm; to be expanded on later

---
+++ commands/Utility/userinfo.js
@@ -0,0 +1,24 @@
+exports.run = async (bot, msg, args) => {
+    const user = msg.mentions.members.first();
+
+    msg.channel.send(
+        new RichEmbed()
+            .setTitle(`Information for ${user.user.tag}`)
+            .addField('User ID', user.id)
+            .addField('Account Creation Date', member.user.createdAt)
+            .addField('Join Guild Date', user.joinedAt)
+            .addField('Ban Count', 'This user has been banned from ' + parseInt(banCounts[user.id]) + ' guilds.')
+    );
+  };
+  
+  exports.conf = {
+    aliases: ['uinfo', 'userinformation'],
+    guildOnly: true,
+  };
+  
+  exports.help = {
+    name: 'userinfo',
+    description: 'Shows information about the mentioned user',
+    usage: '@user',
+  };
+  
