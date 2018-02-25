51192d170d1d896e14603ca0ce5b34b31c6c380e
pokesudenk <thisis@notarealema.il>
2018-02-24 20:12:04 -0500

    added display names

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -8,7 +8,7 @@ exports.run = async (bot, msg) => {
       .addField('User ID', user.id)
       .addField('Account Creation Date', user.user.createdAt)
       .addField('Join Guild Date', user.joinedAt)
-      .addField('Ban Count', 'The user has been banned on ' + parseInt(banCounts[user.id]) + ' guilds.')
+      .addField('Names', 'Display Name: ' + user.displayName + '\nUsername: ' + user.tag)
   );
 };
 
