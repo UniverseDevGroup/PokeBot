1ef65bd505c67c78c0c2e7e9b246fde661cf1ea8
Unknown <jtsshieh@outlook.com>
2018-02-24 22:19:33 -0500

    Gym

--- commands/Pokemon/claim.js
+++ commands/Pokemon/claim.js
@@ -2,11 +2,21 @@ exports.run = async (bot, msg) => {
   if (!msg.channel.name.startsWith('gym-')) return msg.reply('Go into one of the gym channels and try again.');
   if (msg.channel.topic == 'Current Owner: *none*') {
     msg.reply('Alright, you have claimed this gym as yours! Be ready to battle anyone who approaches you');
-    msg.channel.setTopic('Current Owner: ' + msg.author.id + '/' + msg.author.tag);
+    let team;
+    if (msg.member.roles.find('name', 'Aqua')) team = 'Aqua';
+    if (msg.member.roles.find('name', 'Rocket')) team = 'Rocket';
+    if (msg.member.roles.find('name', 'Magma')) team = 'Magma';
+    if (!team) return msg.reply('You have to join a team before you can claim a gym.');
+    msg.channel.setTopic('Current Owner: ' + msg.author.id + '/' + msg.author.tag + '/' + team);
   }
   else {
-
+    let team;
+    if (msg.member.roles.find('name', 'Aqua')) team = 'Aqua';
+    if (msg.member.roles.find('name', 'Rocket')) team = 'Rocket';
+    if (msg.member.roles.find('name', 'Magma')) team = 'Magma';
+    if (!team) return msg.reply('You have to join a team before you can claim a gym.');
     const owner = msg.channel.topic.slice(15).substring(0, 18);
+    if (msg.guild.members.find('id', owner).roles.find('name', team)) return msg.reply('Were you looking to add a pokemon to the gym?');
     msg.channel.send('<@' + owner + '>, come here as ' + msg.member.displayName + ' wants to battle you.');
   }
 };
