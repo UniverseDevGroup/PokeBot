e9f85ec2d5ac613950e93674b8f79d8018d08dc9
pokesudenk <thisis@notarealema.il>
2018-02-24 19:55:49 -0500

    vscode fucks files

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-exports.run = async (bot, msg) => {
-  const user = msg.mentions.members.first();
-  const { RichEmbed } = require('discord.js');
-  msg.channel.send(
-    new RichEmbed()
-      .setTitle(`Information for ${user.user.tag}`)
-      .addField('User ID', user.id)
-      .addField('Account Creation Date', user.user.createdAt)
-      .addField('Join Guild Date', user.joinedAt)
-  );
-};
-=======
 exports.run = async (bot, msg, args) => {
     const Discord = require('discord.js');
     const user = msg.mentions.members.first();
->>>>>>> master
 
 exports.conf = {
   aliases: ['uinfo', 'userinformation'],
