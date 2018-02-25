a5d68950de72f8dd3d7737d97b41c2b13b76e0e9
Unknown <jtsshieh@outlook.com>
2018-02-24 21:35:12 -0500

    testing

--- commands/Pokemon/claim.js
+++ commands/Pokemon/claim.js
@@ -2,10 +2,11 @@ exports.run = async (bot, msg) => {
   if (!msg.channel.topic.startsWith('Current Owner:')) return msg.reply('Go into one of the gym channels and try again.');
   if (msg.channel.topic == 'Current Owner: *none*') {
     msg.reply('Alright, you have claimed this gym as yours! Be ready to battle anyone who approaches you');
-    msg.channel.setTopic('Current Owner: ' + msg.author.tag + '/' + msg.author.id);
+    msg.channel.setTopic('Current Owner: ' + msg.author.id + '/' + msg.author.tag);
   }
   else {
-    const owner = msg.channel.topic.slice(15).substring(msg.author.tag.length - 1);
+
+    const owner = msg.channel.topic.slice(15).substring(19);
     msg.channel.send('<@' + owner + '>, come here as ' + msg.member.displayName + ' wants to battle you.');
   }
 };
