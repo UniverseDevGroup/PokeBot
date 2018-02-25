8d824ae8b51f85e1b6b7fe9f0b6dcf0a2b00b23f
pokesudenk <thisis@notarealema.il>
2018-02-25 00:31:51 -0500

    new, kinda long story

--- commands/Fun/story.js
+++ commands/Fun/story.js
@@ -10,6 +10,7 @@ exports.run = async (bot, msg) => {
     [
       `${selectedUser1.username} bought ${selectedUser2.username} his favorite video game. This game is called "Pokemon". Then, they became best friends.`,
       `${selectedUser1.username} wants to become a Pokemon trainer, but he needs to get a Pokemon first!`,
+      `One day, ${selectedUser1.username} decided to go to a shop. He took his motorbike. Once he arrived at the shop, he went inside. He headed right, towards the chips. He grabbed a packet, and headed to check-out. He bumped into ${selectedUser2.username} at the counter, and decided to say hi. They had a little chat, and then decided to gossip about ${selectedUser3.username}. Then, as they were saying some horrible things about ${selectedUser3.username}, they unexpectedly showed up! Then, ${selectedUser1.username} and ${selectedUser2.username} got into a fight against ${selectedUser3.username}.\n\nKids, this is why you don't start drama.\nNow ${selectedUser1.username}, ${selectedUser2.username}, and ${selectedUser3.username} are no longer friends.`
     ];
     const storySelected = [Math.floor(Math.random() * stories.length)];
     const { RichEmbed } = require('discord.js');
