f7c9219876840a69ac5e3f3960b464bd273512b5
pokesudenk <thisis@notarealema.il>
2018-02-24 19:57:05 -0500

    unrape

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -1,6 +1,14 @@
-exports.run = async (bot, msg, args) => {
-    const Discord = require('discord.js');
-    const user = msg.mentions.members.first();
+exports.run = async (bot, msg) => {
+  const user = msg.mentions.members.first();
+  const { RichEmbed } = require('discord.js');
+  msg.channel.send(
+    new RichEmbed()
+      .setTitle(`Information for ${user.user.tag}`)
+      .addField('User ID', user.id)
+      .addField('Account Creation Date', user.user.createdAt)
+      .addField('Join Guild Date', user.joinedAt)
+  );
+};
 
 exports.conf = {
   aliases: ['uinfo', 'userinformation'],
