54478d30cbc9c2abddde4df2980e45071bddd60e
Unknown <jtsshieh@outlook.com>
2018-02-28 16:50:51 -0500

    test

--- commands/Getting Started/start.js
+++ commands/Getting Started/start.js
@@ -25,7 +25,7 @@ exports.run = (bot, msg) => {
     new RichEmbed()
       .setColor(0x00ae86)
       .setTitle('Getting Started in PokeWorld')
-      .addField('PokeCord', 'First off, we need to get you up and running in PokeCord!\n\nFirst, make sure to use the `p!start` command to pick a starter pokemon. Once you have done that, you can start using PokeCord related features in the server!')
+      .addField('PokeCord', 'First off, we need to get you up and running in PokeCord!\n\nMake sure to use the `p!start` command to pick a starter pokemon. Once you have done that, you can start using PokeCord related features in the server!')
       .addField('Teams // Gyms', 'Next, you may join a team using PokeBot. To do this, run the `join` command followed by the team you want to join. You can join `aqua`, `magma`, and `rocket`. Now, you can start claiming gyms. To do this, run `p:claim` in a gym channel. If no one owns it, the gym will now be yours. If it is not yours, it will either tell you to lay off (if your team owns the gym), or it will ping the owner of the gym and tell them to battle you. If you lose said battle, you will not recieve the gym. Otherwise, the owner of the gym will have to drop it for you to claim. You can also trade gyms with other members using the `tradegym` command.')
       .addField('The End of the Introduction', 'Now that you have done all of this, you are ready to start your journey in the digital region! Chat with people, make friends, and most importantly, have fun!\n\n-The official "sentience" of the PokeWorld server!')
       .setFooter('PokeBot Beta')
