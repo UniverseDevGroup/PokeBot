c038770139edc75eb067096688581baf98f8c00f
Unknown <jtsshieh@outlook.com>
2018-03-02 16:15:35 -0500

    documenting

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,3 +1,23 @@
+/** **************************************
+ *
+ *   GuildMemberRemove: Plugin for PokeBot that waves bye to a user who leaves.
+ *   Copyright (C) 2018 TheEdge, jtsshieh, Alee
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *************************************/
+
 module.exports = (bot, member) => {
   const { RichEmbed } = require('discord.js');
   bot.channels.find('name', 'welcome').send(`**${member.user.tag}** just left. We now have ${member.guild.memberCount} members left. Aww man...`);
