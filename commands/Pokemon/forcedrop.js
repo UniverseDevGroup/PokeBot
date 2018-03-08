9d35ad9327333304002c591ca7eeb18d293a32ee
pokesudenk <thisis@notarealema.il>
2018-03-07 19:06:43 -0500

    new plugin

---
+++ commands/Pokemon/forcedrop.js
@@ -0,0 +1,44 @@
+/** **************************************
+ *
+ *   Ban: Plugin for PokeBot that powers the PokeWorld gym system.
+ *   Copyright (C) 2018 TheEdge, jtsshieh, Alee
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *************************************/
+
+exports.run = async (bot, msg, args) => {
+    if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You don\'t have permssion to ban members.');
+  
+    if (msg.guild.id != '417088992329334792') return msg.reply ('This is a PokeWorld exclusive command. Sorry!');
+    if (!msg.channel.name.startsWith('gym-')) return msg.reply('Go into one of the gym channels and try again.');
+    if (msg.channel.topic == 'Current Owner: *none*') {
+        msg.reply('This gym does not have an owner.');
+    }
+    else {
+        msg.channel.setTopic('Current Owner: *none*');
+        msg.channel.send('You have dropped the gym.');
+    };
+  };
+  
+  exports.conf = {
+    aliases: [],
+    guildOnly: true,
+  };
+  
+  exports.help = {
+    name: 'forcedrop',
+    description: 'Force a gym to have no owner.',
+  };
+  
