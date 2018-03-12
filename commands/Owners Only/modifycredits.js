a462090b26b6978bb0d6259d01f3b86952abfc3a
TheEdgeNK <32907415+TheEdgeNK@users.noreply.github.com>
2018-03-11 22:41:54 -0400

    i am big dumb

--- commands/Owners Only/modifycredits.js
+++ commands/Owners Only/modifycredits.js
@@ -18,7 +18,7 @@
  *
  * *************************************/
 
-exports.run = async (bot, msg) => {
+exports.run = async (bot, msg, args) => {
     if (!['242775871059001344', '247221105515823104', '236279900728721409'].includes(msg.author.id)) return msg.reply('Nope! You need the person who created this bot to use this command.');
     
     let user;
