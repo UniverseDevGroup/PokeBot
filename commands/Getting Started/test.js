26057ebec55651f94dde109e9e71047c62caffa4
Unknown <jtsshieh@outlook.com>
2018-03-16 16:15:09 -0400

    uh oh, hold on

--- commands/Getting Started/test.js
+++ commands/Getting Started/test.js
@@ -1,6 +1,6 @@
 /** **************************************
  *
- *   GuildMemberAdd: Plugin for PokeBot that setups up and welcomes any new user.
+ *   Test: Plugin for PokeBot that helps us test new features
  *   Copyright (C) 2018 TheEdge, jtsshieh, Alee
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -18,33 +18,7 @@
  *
  * *************************************/
 
-module.exports = async (bot, member) => {
-  const { RichEmbed } = require('discord.js');
-  const logChannel = await bot.plugins.settings.getStr('logs', member.guild.id);
-  bot.channels.find('id', logChannel).send(
-    new RichEmbed()
-      .setColor(0x00ae86)
-      .setTitle(`:arrow_right: ${member.user.tag}`)
-      .setDescription(`*${member.user.tag}* joined this server.`)
-      .addField('ID', member.id, true)
-      .addField('Created Account', member.user.createdAt, true)
-      .setTimestamp()
-      .setFooter(member.user.tag, member.user.avatarURL)
-  );
-  if (member.guild.id != '417088992329334792') return;
-  try {
-    draw(bot, member);
-  }
-  catch (err)
-  {
-    console.error(err.stack);
-  }
-  //bot.channels.find('name', 'welcome').send(`Welcome to the server **${member.user.tag}**! Make sure to read the rules! We now have ${member.guild.memberCount} members.`);
-  //const role = member.guild.roles.find('name', 'Trainers');
-  //member.addRole(role);
-};
-
-async function draw(bot, member) {
+exports.run = async (bot, msg) => {
   const Canvas = require('canvas');
   const request = require('request-promise');
   Canvas.registerFont('./assets/Ketchum.otf', {
@@ -58,38 +32,48 @@ async function draw(bot, member) {
   const fs = require('fs');
 
   avatar.src = await request({
-    uri: member.user.avatarURL,
+    uri: msg.author.avatarURL,
     encoding: null
   });
-  base.src = await fs.readFileSync('./assets/Pokemon_Server_Template.png');
+  base.src = await fs.readFileSync('./assets/Pokemon_Welcome_Template.png');
   ctx.drawImage(base, 0, 0, 1500, 500);
 
-  //tag
-  ctx.font = '96px Ketchum';
+  ctx.font = '100px Ketchum';
   ctx.fillStyle = '#e5da2a';
   ctx.strokeStyle = '#3b4cca';
-  ctx.fillText(member.user.tag, 500, 200);
-  ctx.strokeText(member.user.tag, 500, 200);
+  ctx.lineWidth = 5;
+  ctx.fillText(msg.author.tag, 475, 175);
+  ctx.strokeText(msg.author.tag, 475, 175);
 
-
-  //guild name
   ctx.font = '55px Ketchum';
   ctx.fillStyle = '#fff';
-  ctx.fillText(member.guild.name, 820, 310);
+  ctx.fillText(msg.guild.name, 800, 325);
 
-  //avatar
+  ctx.font = '40px Ketchum';
+  ctx.fillStyle = '#fff';
+  ctx.fillText(msg.guild.memberCount + ' members', 100, 70);
+
   ctx.globalAlpha = 1;
   ctx.beginPath();
   ctx.arc(208, 267, 166, 0, 2 * Math.PI, true);
   ctx.closePath();
   ctx.clip();
   ctx.drawImage(avatar, 43, 101, 329, 331);
-
-  return bot.channels.find('name', 'welcome').send({
+  return msg.channel.send({
     files: [{
       attachment: canvas.toBuffer(),
-      name: 'profile.png'
+      name: 'welcomeCard.png'
     }
     ]
   });
-}
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+
+exports.help = {
+  name: 'test',
+  description: 'Introduces you to the PokeWorld server!',
+};
