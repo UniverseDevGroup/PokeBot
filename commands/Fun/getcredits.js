63a779273661f95b461b10516db25fde5cb2aaab
Unknown <jtsshieh@outlook.com>
2018-03-18 20:57:00 -0400

    advertising

--- commands/Fun/getcredits.js
+++ commands/Fun/getcredits.js
@@ -20,16 +20,19 @@
 const commandCooldown = new Set();
 
 exports.run = (bot, msg) => {
-  if (commandCooldown.has(msg.author.id)) return msg.reply('You have claimed credits too recently')
-  
-  
+  if (commandCooldown.has(msg.author.id)) return msg.reply('You have claimed credits too recently');
+
+  if (bot.dbl.hasVoted(msg.author.id)) {
+    bot.plugins.economy.add(msg.author.id, 100);
+    return msg.reply('Since you upvoted the bot, here is 100 credits.');
+  }
   bot.plugins.economy.add(msg.author.id, 25);
   msg.reply('Added 25 credits.');
 
   commandCooldown.add(msg.author.id);
-    setTimeout(() => {
+  setTimeout(() => {
     commandCooldown.delete(msg.author.id);
-    }, 3600000);
+  }, 3600000);
 };
 
 exports.conf = {
