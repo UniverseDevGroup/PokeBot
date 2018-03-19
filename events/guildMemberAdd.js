6d186898e8d26420b7af7ff823aca6fde41b0a7d
Unknown <jtsshieh@outlook.com>
2018-03-18 21:40:07 -0400

    trying something new

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -32,66 +32,65 @@ module.exports = async (bot, member) => {
       .setFooter(member.user.tag, member.user.avatarURL)
   );
   if (member.guild.id != '417088992329334792') return;
-  //try {
-    //draw(bot, member);
-  //}
-  //catch (err)
-  //{
-    //console.error(err.stack);
-  //}
+  try {
+    draw(bot, member);
+  }
+  catch (err)
+  {
+    bot.Raven.captureException(err);
+  }
   bot.channels.find('name', 'welcome').send(`Welcome to the server **${member.user.tag}**! Make sure to read the rules! We now have ${member.guild.memberCount} members.`);
   const role = member.guild.roles.find('name', 'Trainers');
   member.addRole(role);
 };
 
-/** ****************************************************************************** 
-* async function draw(bot, member) {
-*  const Canvas = require('canvas');
-*  const request = require('request-promise');
-*  Canvas.registerFont('./assets/Ketchum.otf', {
-*    family: 'Ketchum'
-*  });
-*  const canvas = Canvas.createCanvas(1500, 500);
-*  const ctx = canvas.getContext('2d');
-*  const Image = Canvas.Image;
-*  const base = new Image();
-*  const avatar = new Image();
-*  const fs = require('fs');
-*
-*  avatar.src = await request({
-*    uri: member.user.avatarURL,
-*    encoding: null
-*  });
-*  base.src = await fs.readFileSync('./assets/Pokemon_Server_Template.png');
-*  ctx.drawImage(base, 0, 0, 1500, 500);
-*
-*  //tag
-*  ctx.font = '96px Ketchum';
-*  ctx.fillStyle = '#e5da2a';
-*  ctx.strokeStyle = '#3b4cca';
-*  ctx.fillText(member.user.tag, 500, 200);
-*  ctx.strokeText(member.user.tag, 500, 200);
-*
-*
-*  //guild name
-*  ctx.font = '55px Ketchum';
-*  ctx.fillStyle = '#fff';
-*  ctx.fillText(member.guild.name, 820, 310);
-*
-*  //avatar
-*  ctx.globalAlpha = 1;
-*  ctx.beginPath();
-*  ctx.arc(208, 267, 166, 0, 2 * Math.PI, true);
-*  ctx.closePath();
-*  ctx.clip();
-*  ctx.drawImage(avatar, 43, 101, 329, 331);
-*
-*  return bot.channels.find('name', 'welcome').send({
-*    files: [{
-*      attachment: canvas.toBuffer(),
-*      name: 'profile.png'
-*    }
-*   ]
-*  });
-* } 
-******************************************************************************* */
+
+async function draw(bot, member) {
+  const Canvas = require('canvas');
+  const request = require('request-promise');
+  Canvas.registerFont('./assets/Ketchum.otf', {
+    family: 'Ketchum'
+  });
+  const canvas = Canvas.createCanvas(1500, 500);
+  const ctx = canvas.getContext('2d');
+  const Image = Canvas.Image;
+  const base = new Image();
+  const avatar = new Image();
+  const fs = require('fs');
+
+  avatar.src = await request({
+    uri: member.user.avatarURL,
+    encoding: null
+  });
+  base.src = await fs.readFileSync('./assets/Pokemon_Welcome_Template.png');
+  ctx.drawImage(base, 0, 0, 1500, 500);
+
+  ctx.font = '100px Ketchum';
+  ctx.fillStyle = '#e5da2a';
+  ctx.strokeStyle = '#3b4cca';
+  ctx.lineWidth = 5;
+  ctx.fillText(member.user.tag, 475, 175);
+  ctx.strokeText(member.user.tag, 475, 175);
+
+  ctx.font = '55px Ketchum';
+  ctx.fillStyle = '#fff';
+  ctx.fillText(member.guild.name, 800, 325);
+
+  ctx.font = '40px Ketchum';
+  ctx.fillStyle = '#fff';
+  ctx.fillText(member.guild.memberCount + ' members', 100, 70);
+
+  ctx.globalAlpha = 1;
+  ctx.beginPath();
+  ctx.arc(208, 267, 166, 0, 2 * Math.PI, true);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(avatar, 43, 101, 329, 331);
+  return bot.channels.find('name', 'welcome').send({
+    files: [{
+      attachment: canvas.toBuffer(),
+      name: 'welcomeCard.png'
+    }
+    ]
+  });
+}
