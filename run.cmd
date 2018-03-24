41e64a4cb59494643805a5c968d8a3c4ff97cd4f
TheEdgeNK <32907415+TheEdgeNK@users.noreply.github.com>
2018-03-24 15:49:13 -0400

    thingy

--- run.cmd
+++ run.cmd
@@ -1,6 +1,19 @@
 @echo off
 
 title PokeBot Console
+
+echo ------------------------------
+echo |Welcome to PokeBot Console! |
+echo |If you ran this without     |
+echo |changing the token, you have|
+echo |made an error. You are      |
+echo |supposed to navigate to the |
+echo |config.json file, and change|
+echo |the token to your bot user's|
+echo |token. After that, you have |
+echo |managed to successfully self|
+echo |host PokeBot!               |
+echo ------------------------------
 :a
 git pull
 node bot.js
