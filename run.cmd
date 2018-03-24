a98266c87f693147f782394eeb5821d8cf6c4fea
TheEdgeNK <32907415+TheEdgeNK@users.noreply.github.com>
2018-03-24 15:51:28 -0400

    JIMP

--- run.cmd
+++ run.cmd
@@ -2,18 +2,16 @@
 
 title PokeBot Console
 
-echo ------------------------------
-echo |Welcome to PokeBot Console! |
-echo |If you ran this without     |
-echo |changing the token, you have|
-echo |made an error. You are      |
-echo |supposed to navigate to the |
-echo |config.json file, and change|
-echo |the token to your bot user's|
-echo |token. After that, you have |
-echo |managed to successfully self|
-echo |host PokeBot!               |
-echo ------------------------------
+echo Welcome to PokeBot Console! 
+echo If you ran this without     
+echo changing the token, you have
+echo made an error. You are      
+echo supposed to navigate to the 
+echo config.json file, and change
+echo the token to your bot user's
+echo token. After that, you have 
+echo managed to successfully self
+echo host PokeBot!              
 :a
 git pull
 node bot.js
