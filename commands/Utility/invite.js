c272bbb64bac1e3129d7ae4e6dac3ec103a2be23
Unknown <jtsshieh@outlook.com>
2018-03-25 19:21:45 -0400

    linting the code

--- commands/Utility/invite.js
+++ commands/Utility/invite.js
@@ -19,16 +19,16 @@
  * *************************************/
 
 exports.run = (bot, msg) => {
-    msg.channel.send('You can invite me by visiting https://discordbots.org/bot/417096530596724737.');
-  };
+  msg.channel.send('You can invite me by visiting https://discordbots.org/bot/417096530596724737.');
+};
   
-  exports.conf = {
-    aliases: [],
-    guildOnly: true,
-  };
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
   
-  exports.help = {
-    name: 'invite',
-    description: 'Gives the user a link to invite the bot.',
-  };
+exports.help = {
+  name: 'invite',
+  description: 'Gives the user a link to invite the bot.',
+};
   
