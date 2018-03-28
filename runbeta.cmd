7b1138803d887e9b435446330e3d804fd9b5bd96
TheEdgeNK <trey6979@gmail.com>
2018-03-28 10:37:38 -0400

    A better console message

--- runbeta.cmd
+++ runbeta.cmd
@@ -2,16 +2,13 @@
 
 title PokeBot Beta Console
 
-echo Welcome to PokeBot Beta Console! 
-echo If you ran this without     
-echo changing the token, you have
-echo made an error. You are      
-echo supposed to navigate to the 
-echo config.json file, and change
-echo the token to your bot user's
-echo token. After that, you have 
-echo managed to successfully self
-echo host PokeBot!              
+echo Welcome to the PokeBot Beta Console.
+echo If you want to self-host this bot, please continue by executing the following steps:
+echo 1. You may need to go into the code and comment out anything that relates to Discord Bot List, as this will cause errors when self-hosting.
+echo 2. Create a config.json file and include the token of your bot user so that the bot does not error out when connecting to Discord Servers.
+echo 3. You will need to edit this file that you ran, and remove the "git pull" line. This causes errors as it will try to pull from the PokeBot git.
+echo 4. Finally, you must credit the developers (Universe Development Group or PokeWorld Administrators will work fine).
+              
 :a
 git pull
 node test.js
