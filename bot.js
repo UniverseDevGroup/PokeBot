8252c0a76dc252f224014016cc6cdd2189536a10
Unknown <jtsshieh@outlook.com>
2018-03-31 21:55:38 -0400

    even more cleanup

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.categories = new Discord.Collection();
 bot.queue = new Discord.Collection();
-bot.plugins = { music : require('./Plugins/Music.js') , economy : require('./Plugins/Economy.js'), settings : require('./Plugins/settings.js'), whitelist: require('./Plugins/whitelist.js')};
+bot.plugins = { music : require('./plugins/music.js') , economy : require('./plugins/Economy.js'), settings : require('./plugins/settings.js'), whitelist: require('./plugins/whitelist.js')};
 cmdLoader();
 
 bot.Raven = require('raven');
