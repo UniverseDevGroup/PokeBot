fc29a5a2b81183b0e99906c7968d3820aee58db7
Trey <32907415+TheEdgeNK@users.noreply.github.com>
2018-08-12 03:55:55 -0400

    fix air or in the Command

--- commands/Utility/recentchanges.js
+++ commands/Utility/recentchanges.js
@@ -8,16 +8,16 @@
  * *************************************/
 module.exports.run = async (client, bot, message) => {
     const { RichEmbed } = require('discord.js');
-    const embed = new RichEmbed();
-
-    embed
-        .setColor (0x36393e)
+    
+    msg.channel.send(
+    new RichEmbed()
+      .setColor (0x36393e)
         .setTitle ("These are the newest features")
         .setDescription ("And fixes too!")
         .setFooter ("Galaxy 2.0 Alpha 1");
-
-    embed.addField ("New Features", "Added the recent changes command. This command tells the user about the newest additions to the bot.\nQuote commands have been added. Heard of Victor? We don't care. Have his quotes anyways! We also have normal quotes from random members of the Universe community too! This comes as the part of a merge between another bot.");
-    embed.addField ("Fixes", "Changed the add1k command name to exploit.\nFixed licensing info")
+        .addField ("New Features", "Added the recent changes command. This command tells the user about the newest additions to the bot.\nQuote commands have been added. Heard of Victor? We don't care. Have his quotes anyways! We also have normal quotes from random members of the Universe community too! This comes as the part of a merge between another bot.");
+        .addField ("Fixes", "Changed the add1k command name to exploit.\nFixed licensing info")
+  );
 
   };
   
@@ -26,9 +26,9 @@ module.exports.run = async (client, bot, message) => {
     guildOnly: false,
   };
   exports.help = {
-    name: 'info',
+    name: 'recentchanges',
     description: 'Tells you information about the bot',
-    usage: 'info',
+    usage: '',
     category: '- Information Commands',
   };
   
